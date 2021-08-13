2e9caaf1a043d1eaed93beb0cf3ac11730c62198
NSLyuma <iinokayo@mail.ru>
2021-08-13 18:51:07 +0300

    Data output with indication of pages. Total value output

--- costs_trying/src/App.vue
+++ costs_trying/src/App.vue
@@ -1,23 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <header>My personal costs</header>
+    <PaymentsDisplay :list="paymentsList" />
+    <div>Total value: {{ totalValue }}</div>
   </div>
 </template>
 
 <script>
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 export default {
   name: "App",
-  components: {},
+  components: { PaymentsDisplay },
+  data() {
+    return {
+      paymentsList: {},
+    };
+  },
+  methods: {
+    fetchData() {
+      return {
+        page1: [
+          {
+            id: 1,
+            date: "13.08.2021",
+            category: "Food",
+            value: 100,
+          },
+          {
+            id: 2,
+            date: "13.08.2021",
+            category: "Food",
+            value: 200,
+          },
+          {
+            id: 3,
+            date: "13.08.2021",
+            category: "Food",
+            value: 300,
+          },
+          {
+            id: 4,
+            date: "13.08.2021",
+            category: "Food",
+            value: 400,
+          },
+          {
+            id: 5,
+            date: "13.08.2021",
+            category: "Food",
+            value: 500,
+          },
+        ],
+        page2: [
+          {
+            id: 6,
+            date: "13.08.2021",
+            category: "Food",
+            value: 100,
+          },
+          {
+            id: 7,
+            date: "13.08.2021",
+            category: "Food",
+            value: 200,
+          },
+          {
+            id: 8,
+            date: "13.08.2021",
+            category: "Food",
+            value: 300,
+          },
+          {
+            id: 9,
+            date: "13.08.2021",
+            category: "Food",
+            value: 400,
+          },
+          {
+            id: 10,
+            date: "13.08.2021",
+            category: "Food",
+            value: 500,
+          },
+        ],
+      };
+    },
+  },
+  computed: {
+    numberOfPages() {
+      return Object.keys(this.paymentsList);
+    },
+    totalValue() {
+      let sum = 0;
+      for (let page in this.paymentsList) {
+        //перебор объекта по ключам
+        let preSum = this.paymentsList[page].reduce(
+          //вычисление суммы каждой страницы
+          (acc, cur) => (acc += cur.value),
+          0
+        );
+        sum += preSum;
+      }
+      return sum;
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
+  },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ costs_trying/src/components/PaymentsDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(page, i) in list" :key="i">
+      {{ i }}:
+      <div v-for="item in page" :key="item.id">
+        {{ item.id }}. {{ item.date }} {{ item.category }}
+        {{ item.value }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    list: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
